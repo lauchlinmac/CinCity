db3b8b3cc2ca179a5aee8a89c6e50d48738c7d88
lauchlinmac <lauchlinmac@gmail.com>
2021-10-13 21:22:51 -0400

    Adding the Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+function Head() {
+    return (
+      <header className="App">
+      <h1>Cin City Running Group</h1>
+      <div class="nav">
+          <ul>
+              <li>Home</li>
+              <li>Races</li>
+              <li>Events</li>
+              <li>About</li>
+          </ul>
+      </div>  
+      </header>
+    );
+  }
+  
+  export default Head;
